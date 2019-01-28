5a528c80909bda8ef7bd25b81f37e3106b1b264a
JaysonAlbert <JaysonAlbert@users.noreply.github.com>
2019-01-28 14:31:17 +0800

    删除冗余代码

--- kog.py
+++ kog.py
@@ -10,11 +10,6 @@ from baseline import check_action
 # 屏幕分辨率
 device_x, device_y = 2244, 1080
 
-# 通关模式：1=重新挑战 -> 挑战界面，2=重新挑战-> 更换阵容
-game_mode = 2
-
-# 各步骤等待间隔
-skip_wait = [10, 51, 16, 18]
 
 # 刷金币次数
 repeat_times = 60
@@ -35,30 +30,6 @@ def tap_screen(x, y):
     os.system(cmd)
 
 
-def do_money_work():
-    global game_mode
-    if game_mode == 1: # 再次挑战
-        logging.info('#0 restart challenge...')
-        tap_screen(1877, 993)
-        sleep(1)
-
-    game_mode = 1
-    logging.info('#1 start match!!!')
-    tap_screen(1600, 888)   # 闯关，更换阵容
-    sleep(1)
-
-    for i in skip_wait:
-        sleep(i)
-        logging.info('skip conversation, interval {}s...'.format(i))
-        tap_screen(2080, 44) # 跳过对话
-
-    sleep(4)
-    tap_screen(1074, 971) # 点击屏幕继续
-
-    logging.info('#3 tap to continue...\n')
-    sleep(3)
-
-
 def take_action(action):
     if not action:
         return
@@ -76,6 +47,3 @@ if __name__ == '__main__':
     while True:
         action = check_action()
         take_action(action)
-    # for i in range(repeat_times):
-    #     logging.info('round #{}'.format(i + 1))
-    #     do_money_work()
