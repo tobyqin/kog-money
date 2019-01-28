ef91ebaff140f17d74680d1b655f2c64c0fb9d63
JaysonAlbert <JaysonAlbert@users.noreply.github.com>
2019-01-28 14:29:36 +0800

    自动判断场景，并采取相应操作

---
+++ baseline.py
@@ -0,0 +1,67 @@
+from PIL import Image
+import numpy as np
+import os
+import logging
+import subprocess
+from io import BytesIO
+from auto_adb import auto_adb
+from adb.client import Client as AdbClient
+client = AdbClient(host="127.0.0.1", port=5037)
+
+adb = auto_adb()
+device = client.devices()[0]
+
+baseline = {}
+
+SCREENSHOT_WAY = 3
+
+boxes = {
+    'restart': (1750, 950, 2000, 1040),
+    'continue': (1000, 940, 1250, 1000),
+    'start': (1475,  830, 1720, 920),
+    'skip0': (2000, 29, 2100, 85),
+    'skip1': (2025, 20, 2135, 70)
+}
+
+threshold = 10
+
+
+ACTIONS = ['skip0', 'skip1', 'start', 'restart', 'continue']
+
+
+def save_crop():
+    for key, val in boxes.items():
+        img = Image.open(key+'.png')
+        img.crop(val).save('crop_'+key+'.png')
+
+def pull_screenshot():
+    global SCREENSHOT_WAY
+
+    if SCREENSHOT_WAY == 0:
+        os.system('adb shell screencap -p /sdcard/screen.png')
+        os.system('adb pull /sdcard/screen.png')
+        return Image.open('screen.png')
+    elif 1 <= SCREENSHOT_WAY <= 3:
+        return Image.open(BytesIO(device.screencap()))
+
+
+def check_action():
+    if not baseline:
+        for n in ACTIONS:
+            baseline[n] = np.array(Image.open('crop_' + n + '.png'))
+
+    frame = pull_screenshot()
+
+    crop_frame = {}
+    for key, val in boxes.items():
+        crop_frame[key] = np.sum(baseline[key] - np.array(frame.crop(val))) / baseline[key].size
+
+    min_key = min(crop_frame, key=crop_frame.get)
+    if crop_frame[min_key] < threshold:
+        logging.info("ACTION: {}".format(min_key))
+        return min_key
+
+    logging.info("NO ACTION")
+    return None
+
+
--- kog.py
+++ kog.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-
+import timeit
 import logging
 import os
 from time import sleep
+from PIL import Image
+from baseline import check_action
 
 # 屏幕分辨率
-device_x, device_y = 1920, 1080
+device_x, device_y = 2244, 1080
 
 # 通关模式：1=重新挑战 -> 挑战界面，2=重新挑战-> 更换阵容
 game_mode = 2
 
 # 各步骤等待间隔
-step_wait = [3, 13, 24, 3, 3]
+skip_wait = [10, 51, 16, 18]
 
 # 刷金币次数
 repeat_times = 60
@@ -20,40 +22,60 @@ repeat_times = 60
 # 日志输出
 logging.basicConfig(format='%(asctime)s %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
 
 def tap_screen(x, y):
     """calculate real x, y according to device resolution."""
-    base_x, base_y = 1920, 1080
+    base_x, base_y = 2244, 1080
     real_x = int(x / base_x * device_x)
     real_y = int(y / base_y * device_y)
-    os.system('adb shell input tap {} {}'.format(real_x, real_y))
+    cmd = 'adb shell input tap {} {}'.format(real_x, real_y)
+    logging.debug(cmd)
+    os.system(cmd)
 
 
 def do_money_work():
-    if game_mode == 1:
-        logging.debug('#0 start the game')
-        tap_screen(1600, 970)
-        sleep(step_wait[0])
-
-    logging.debug('#1 ready, go!!!')
-    tap_screen(1450, 910)
-    sleep(step_wait[1])
-
-    logging.debug('#2 auto power on!')
-    tap_screen(1780, 40)
-
-    for i in range(step_wait[2]):
-        tap_screen(1720, 80)
+    global game_mode
+    if game_mode == 1: # 再次挑战
+        logging.info('#0 restart challenge...')
+        tap_screen(1877, 993)
         sleep(1)
 
-    logging.debug('#3 do it again...\n')
-    tap_screen(1600, 980)
-    sleep(step_wait[4])
+    game_mode = 1
+    logging.info('#1 start match!!!')
+    tap_screen(1600, 888)   # 闯关，更换阵容
+    sleep(1)
+
+    for i in skip_wait:
+        sleep(i)
+        logging.info('skip conversation, interval {}s...'.format(i))
+        tap_screen(2080, 44) # 跳过对话
+
+    sleep(4)
+    tap_screen(1074, 971) # 点击屏幕继续
+
+    logging.info('#3 tap to continue...\n')
+    sleep(3)
+
+
+def take_action(action):
+    if not action:
+        return
+    if action == 'restart':
+        tap_screen(1877, 993)
+    elif action == 'start':
+        tap_screen(1600, 888)
+    elif action in ['skip0', 'skip1']:
+        tap_screen(2080, 44)
+    elif action == 'continue':
+        tap_screen(1074, 971)
 
 
 if __name__ == '__main__':
-    for i in range(repeat_times):
-        logging.info('round #{}'.format(i + 1))
-        do_money_work()
+    while True:
+        action = check_action()
+        take_action(action)
+    # for i in range(repeat_times):
+    #     logging.info('round #{}'.format(i + 1))
+    #     do_money_work()
